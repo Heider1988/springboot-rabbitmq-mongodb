4cc1a1a7adab2ad8c4c89d39127ac73b53b9aae6
Heider Oliveira <heider.o@icloud.com>
2025-05-17 15:30:17 -0300

    feat: initialize Spring Boot project with RabbitMQ and MongoDB support
    - Added Maven Wrapper for build consistency
    - Configured dependencies for Spring Boot, RabbitMQ, and MongoDB
    - Setup initial application structure and entry point
    - Included basic test configuration and `contextLoads` test
    - Added `.gitignore` for common IDE and build files

---
+++ src/main/java/com/api/rabbitmq/rabbitmqmongodb/RabbitmqMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.api.rabbitmq.rabbitmqmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqMongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/rabbitmq/rabbitmqmongodb/RabbitmqMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.rabbitmq.rabbitmqmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
